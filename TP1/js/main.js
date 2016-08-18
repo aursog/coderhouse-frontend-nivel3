60184f453e50b94855f3fa48ed40f9744047d22d
Anggelo Urso Goddard <aurso@cl-aurso-2.local>
2016-08-17 21:59:50 -0300

    Se agregan cambios en CV

--- TP1/js/main.js
+++ TP1/js/main.js
@@ -5,14 +5,14 @@
 var app = angular.module("mycv", []);
 
 app.controller("DatosPersonales", function($scope){
-    var p = {
+    var persona = {
         nombre: "Anggelo Urso G.",
         titulo: "Ingeniero Ejecución Informática",
         casa_estudio: "UTFSM",
         pais: "Chile"
-    }
+    };
 
-    $scope.persona = p;
+    $scope.persona = persona;
 
     $scope.openEditInput = function() {
         // Primero escondemos inputs
@@ -20,7 +20,32 @@ app.controller("DatosPersonales", function($scope){
         $('#titulo-persona').addClass("ng-hide");
         $('#editButton').addClass("ng-hide");
 
-        
+        // Segundo, desplegamos lo que nos interesa
+        $('#nombre-persona-input').removeClass("ng-hide");
+        $('#titulo-persona-input').removeClass("ng-hide");
+        $('#saveButton').removeClass("ng-hide");
     }
+
+    $scope.saveChanges = function() {
+        // Primero escondemos inputs
+        $('#nombre-persona').removeClass("ng-hide");
+        $('#titulo-persona').removeClass("ng-hide");
+        $('#editButton').removeClass("ng-hide");
+
+        // Segundo, desplegamos lo que nos interesa
+        $('#nombre-persona-input').addClass("ng-hide");
+        $('#titulo-persona-input').addClass("ng-hide");
+        $('#saveButton').addClass("ng-hide");
+    }
+});
+
+app.controller("DatosLaborales", function($scope){
+    var laburos = [];
+
+    $scope.saveLaburo = function() {
+        laburos.push($scope.new_laburo);
+    }
+
+    $scope.laburos = laburos;
 });
 
